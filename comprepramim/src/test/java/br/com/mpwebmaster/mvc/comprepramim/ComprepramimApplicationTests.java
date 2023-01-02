68ec802fffdb5553ae6fda314fda86abafbd06e3
Marcos Paulo <marqquinhoo@hotmail.com>
2023-01-02 14:26:31 -0300

    Versao 1.0.0

---
+++ comprepramim/src/test/java/br/com/mpwebmaster/mvc/comprepramim/ComprepramimApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpwebmaster.mvc.comprepramim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprepramimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
